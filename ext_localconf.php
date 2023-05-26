87728d3eab384460e06a486b1feec20d3c92df86
Tanel Põld <tanel@brightside.ee>
2023-05-26 09:35:31 +0300

    [TASK] Wrap it into a function

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,11 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die('Access denied.');
 
-if(!class_exists('\MatthiasMullie\Minify')){
-    $composerAutoloadFile = ExtensionManagementUtility::extPath('embedassets')
-        . 'Resources/Private/PHP/autoload.php';
+(function () {
+    if(!class_exists('\MatthiasMullie\Minify')){
+        $composerAutoloadFile = ExtensionManagementUtility::extPath('embedassets')
+            . 'Resources/Private/PHP/autoload.php';
 
-    require_once($composerAutoloadFile);
+        require_once($composerAutoloadFile);
+    }
 }
