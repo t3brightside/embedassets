f0741b4fff4812de99720fd2733603b5ed18f86d
Tanel Põld <tanel@brightside.ee>
2023-05-26 09:39:04 +0300

    [TASK] Add strict_types remove function

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,14 @@
 <?php
+declare(strict_types=1);
+
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die('Access denied.');
 
-(function () {
-    if(!class_exists('\MatthiasMullie\Minify')){
-        $composerAutoloadFile = ExtensionManagementUtility::extPath('embedassets')
-            . 'Resources/Private/PHP/autoload.php';
+if(!class_exists('\MatthiasMullie\Minify')){
+    $composerAutoloadFile = ExtensionManagementUtility::extPath('embedassets')
+        . 'Resources/Private/PHP/autoload.php';
 
-        require_once($composerAutoloadFile);
-    }
+    require_once($composerAutoloadFile);
 }
+
